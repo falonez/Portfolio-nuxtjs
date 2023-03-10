2f688eb932520ee57825868b4f5929eff47bf923
Fathan Mubarok <falonez01@gmail.com>
2023-03-10 11:34:55 +0700

    Create Component Navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div
+    class="flex justify-between items-center px-[10%] py-8 fixed w-screen top-0 left-0 right-0 text-white bg-slate-600 z-20"
+  >
+    <nuxt-link to="/" class="text-[1rem] md:text-2xl">Fathan Mubarok</nuxt-link>
+    <div class="text-2xl md:text-4xl text-white flex items-center">
+      <Icon name="mdi:github" />
+      <Icon name="mdi:linkedin" />
+      <Icon name="mdi:instagram" />
+    </div>
+  </div>
+</template>
