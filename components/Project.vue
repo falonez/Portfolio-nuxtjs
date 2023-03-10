975ffa1b67be8838d92ce5bc7fe3af0f2f0a3180
Fathan Mubarok <falonez01@gmail.com>
2023-03-10 11:35:17 +0700

    Create Component Project

---
+++ components/Project.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="p-[10%] w-full h-screen">
+    <h1 class="text-2xl text-white mb-4">Project</h1>
+    <div class="grid grid-cols-3 gap-8">
+      <div class="p-4 bg-slate-500 rounded-lg space-y-2 text-white">
+        <h1>Flywithme</h1>
+        <h1>Aplikasi pemesanan tiket Pesawat</h1>
+        <img src="@/assets/flywithme/Cover.png" alt="" />
+      </div>
+      <div class="p-4 bg-slate-500 rounded-lg space-y-2 text-white">
+        <h1>Roxy Pharmacy</h1>
+        <h1>Aplikasi pengelolaan Apotek</h1>
+        <img src="@/assets/roxy/cover.png" alt="" />
+      </div>
+      <div class="p-4 bg-slate-500 rounded-lg space-y-2 text-white">
+        <h1>Letter Submission</h1>
+        <h1>Aplikasi Pengajuan Tiket</h1>
+        <img src="@/assets/upi/1.png" alt="" />
+      </div>
+    </div>
+  </div>
+</template>
