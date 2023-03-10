3c8c91ed8c4e66b2a86a3c54e9c6a375d802f839
Fathan Mubarok <falonez01@gmail.com>
2023-03-10 11:34:33 +0700

    Create Component Experience

---
+++ components/Experience.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="px-[10%] pt-[10%] h-screen">
+    <h1 class="text-white text-2xl">Experience</h1>
+    <div class="w-full md:w-1/2 h-auto md:h-full">
+      <ul
+        class="p-5 bg-slate-500 mt-4 relative flex flex-col-reverse text-slate-300 rounded-lg"
+      >
+        <li class="flex flex-row items-center group relative">
+          <div
+            class="w-2 h-2 bg-slate-200 mr-2 group-hover:bg-teal-400 group-hover:animate-spin invisible group-hover:visible"
+          ></div>
+
+          <a href="/" class="group-hover:text-teal-300"
+            >Praktek Kerja Lapangan</a
+          >
+          <div
+            class="w-full bottom-0 rounded-md bg-slate-200 absolute md:-right-full invisible group-hover:visible p-4 text-slate-500"
+          >
+            <h1 class="text-slate-900">Praktek Kerja Lapangan</h1>
+            <p>
+              Pada saat sekolah saya mengikuti kegiatan Praktek Kerja Lapangan
+              di Universitas Pendidikan Indonesia (UPI) dengan mengikuti
+              kegiatan pembuatan website untuk Universitas Pendidikan Indonesia
+              (UPI) dengan menggunakan framework Codeigniter
+            </p>
+          </div>
+        </li>
+        <li class="flex flex-row items-center group relative">
+          <div
+            class="w-2 h-2 bg-slate-200 mr-2 group-hover:bg-teal-400 group-hover:animate-spin invisible group-hover:visible"
+          ></div>
+          <a href="/" class="group-hover:text-teal-300"
+            >Chairman of the MPK Organization</a
+          >
+          <div
+            class="w-full bottom-0 rounded-md bg-slate-200 absolute md:-right-full invisible group-hover:visible p-4 text-slate-500"
+          >
+            <h1 class="text-slate-900">MPK Organization</h1>
+            <p>
+              Ketika saya sekolah di SMK Angkasa 1 Margahayu, saya pernah
+              menjadi ketua organisasi MPK (Majelis Perusyawaratan Kelas) yang
+              mana bertugas mengatur kegiatan-kegiatan di sekolah bersama osis
+            </p>
+          </div>
+        </li>
+        <li class="flex flex-row items-center group relative">
+          <div
+            class="w-2 h-2 bg-slate-200 mr-2 group-hover:bg-teal-400 group-hover:animate-spin invisible group-hover:visible"
+          ></div>
+          <a href="/" class="group-hover:text-teal-300"
+            >Full-Stack Web Development Student</a
+          >
+          <div
+            class="w-full bottom-0 rounded-md bg-slate-200 absolute md:-right-full invisible group-hover:visible p-4 text-slate-500"
+          >
+            <h1 class="text-slate-900">Binar Academy ~ Full-Stack Web</h1>
+            <p>
+              Pada bulan Agustus saya mengikuti program kampus merdeka yaitu
+              Studi Independent dan mengambil learning path Fullstack Web
+              Developer di Binar Academy dan membangun project Flywithme yaitu
+              aplikasi pemesanan tiket pesawat
+            </p>
+          </div>
+        </li>
+        <li class="flex flex-row items-center group relative">
+          <div
+            class="w-2 h-2 bg-slate-200 mr-2 group-hover:bg-teal-400 group-hover:animate-spin invisible group-hover:visible"
+          ></div>
+          <a href="/" class="group-hover:text-teal-300">Frontend Developer</a>
+          <div
+            class="w-full bottom-0 rounded-md bg-slate-200 absolute md:-right-full invisible group-hover:visible p-4 text-slate-500"
+          >
+            <h1 class="text-slate-900">
+              PT Mahakarya Nuansa Mandiri ~ Frontend Developer
+            </h1>
+            <p>
+              Pada bulan November saya magang di jakarta bertempat di menara 165
+              denngan role sebagai Frontend Developer dengan project Roxy
+              Pharmacy yaitu membangun aplikasi pengelolaan apotek
+            </p>
+          </div>
+        </li>
+        <li class="flex flex-row items-center group md:relative">
+          <div
+            class="w-2 h-2 bg-slate-200 mr-2 group-hover:bg-teal-400 group-hover:animate-spin invisible group-hover:visible"
+          ></div>
+          <a href="/" class="group-hover:text-teal-300">Software Engineer</a>
+          <div
+            class="w-full bottom-0 rounded-md bg-slate-200 absolute md:-right-full invisible group-hover:visible p-4 text-slate-500"
+          >
+            <h1 class="text-slate-900">
+              PT GITS INDONESISA ~ Software Engineer
+            </h1>
+            <p>
+              Pada tanggal 13 bulan Februari saya mendapatkan Offering dari
+              website kampus merdeka dari program MSIB batch 4
+            </p>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
