065d8f22b471fdb9bd695ee3d115d756e8daa0c7
Fathan Mubarok <falonez01@gmail.com>
2023-03-15 11:46:19 +0700

    Update Card

--- components/Project.vue
+++ components/Project.vue
@@ -3,20 +3,64 @@
     <h1 class="text-2xl text-white mb-4">Project</h1>
     <div data-aos="fade-up" data-aos-anchor-placement="center-bottom">
       <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
-        <div class="p-4 bg-slate-500 rounded-lg space-y-2 text-white">
-          <h1>Flywithme</h1>
-          <h1>Aplikasi pemesanan tiket Pesawat</h1>
-          <img src="@/assets/flywithme/Cover.png" alt="" />
+        <div
+          class="bg-slate-500 rounded-lg space-y-2 text-white group relative"
+        >
+          <div
+            class="group-hover:before:opacity-0 before:duration-1000 transition-all before:contents-[''] before:absolute before:w-full before:h-full before:opacity-80 before:rounded-lg before:bg-black before:z-10 z-0 top-0 right-0"
+          >
+            <img
+              class="rounded-tl-lg rounded-tr-lg"
+              src="@/assets/flywithme/Cover.png"
+              alt=""
+            />
+          </div>
+          <div class="p-4">
+            <h1 class="group-hover:text-white text-slate-500">Flywithme</h1>
+            <h1 class="group-hover:text-white text-slate-500">
+              Aplikasi pemesanan tiket Pesawat
+            </h1>
+          </div>
         </div>
-        <div class="p-4 bg-slate-500 rounded-lg space-y-2 text-white">
-          <h1>Roxy Pharmacy</h1>
-          <h1>Aplikasi pengelolaan Apotek</h1>
-          <img src="@/assets/roxy/cover.png" alt="" />
+        <div
+          class="bg-slate-500 rounded-lg space-y-2 text-white group relative"
+        >
+          <div
+            class="group-hover:before:opacity-0 before:duration-1000 transition-all before:contents-[''] before:absolute before:w-full before:h-full before:opacity-80 before:rounded-lg before:bg-black before:z-10 z-0 top-0 right-0"
+          >
+            <img
+              class="rounded-tl-lg rounded-tr-lg"
+              src="@/assets/upi/1.png"
+              alt=""
+            />
+          </div>
+          <div class="p-4">
+            <h1 class="group-hover:text-white text-slate-500">
+              Aplikasi Pengajuan Tiket
+            </h1>
+            <h1 class="group-hover:text-white text-slate-500">
+              Letter Submission
+            </h1>
+          </div>
         </div>
-        <div class="p-4 bg-slate-500 rounded-lg space-y-2 text-white">
-          <h1>Letter Submission</h1>
-          <h1>Aplikasi Pengajuan Tiket</h1>
-          <img src="@/assets/upi/1.png" alt="" />
+        <div
+          class="bg-slate-500 rounded-lg space-y-2 text-white group relative"
+        >
+          <div
+            class="group-hover:before:opacity-0 before:duration-1000 transition-all before:contents-[''] before:absolute before:w-full before:h-full before:opacity-80 before:rounded-lg before:bg-black before:z-10 z-0 top-0 right-0"
+          >
+            <img
+              class="rounded-tl-lg rounded-tr-lg"
+              src="@/assets/roxy/cover.png"
+              alt=""
+            />
+          </div>
+          <div class="p-4">
+            <h1 class="group-hover:text-white text-slate-500">Roxy Pharmacy</h1>
+            <h1 class="group-hover:text-white text-slate-500">
+              Aplikasi pengelolaan Apotek
+            </h1>
+          </div>
         </div>
       </div>
     </div>
