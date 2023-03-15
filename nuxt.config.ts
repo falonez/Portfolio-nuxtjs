2b1dda776c85b9eddb98776b2b0b8314f63c01e4
Fathan Mubarok <falonez01@gmail.com>
2023-03-15 11:13:42 +0700

    Add Package AOS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@gits-id/ui-nuxt", "@nuxtjs/tailwindcss", "nuxt-icon"],
+  plugins: [{ src: "~/plugins/aos.ts", mode: "client" }],
   typescript: {
     shim: false,
   },
