67b57ef4c511e85fa228de2e52fdc8389e3cdbe7
Fathan Mubarok <falonez01@gmail.com>
2023-03-15 12:58:49 +0700

    Add Color in Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,38 @@
-const colors = require('tailwindcss/colors');
+const colors = require("tailwindcss/colors");
 
 // default GITS Color
 const primary = {
-  DEFAULT: '#28A0F6',
-  50: '#D8EEFD',
-  100: '#C5E5FD',
-  200: '#9DD4FB',
-  300: '#76C3F9',
-  400: '#4FB1F8',
-  500: '#28A0F6',
-  600: '#0984DD',
-  700: '#0764A7',
-  800: '#054471',
-  900: '#02233B',
+  DEFAULT: "#28A0F6",
+  50: "#D8EEFD",
+  100: "#C5E5FD",
+  200: "#9DD4FB",
+  300: "#76C3F9",
+  400: "#4FB1F8",
+  500: "#28A0F6",
+  600: "#0984DD",
+  700: "#0764A7",
+  800: "#054471",
+  900: "#02233B",
 };
 
 // default GITS Color
 const secondary = {
-  DEFAULT: '#FF8B49',
-  50: '#FFFFFF',
-  100: '#FFF3EC',
-  200: '#FFD9C3',
-  300: '#FFBF9B',
-  400: '#FFA572',
-  500: '#FF8B49',
-  600: '#FF6711',
-  700: '#D84E00',
-  800: '#A03A00',
-  900: '#682600',
+  DEFAULT: "#FF8B49",
+  50: "#FFFFFF",
+  100: "#FFF3EC",
+  200: "#FFD9C3",
+  300: "#FFBF9B",
+  400: "#FFA572",
+  500: "#FF8B49",
+  600: "#FF6711",
+  700: "#D84E00",
+  800: "#A03A00",
+  900: "#682600",
 };
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./node_modules/@gits-id/**/src/**/*.{vue,js,ts,jsx,tsx,css}'],
+  content: ["./node_modules/@gits-id/**/src/**/*.{vue,js,ts,jsx,tsx,css}"],
   theme: {
     extend: {
       colors: {
@@ -42,8 +42,9 @@ module.exports = {
         success: colors.emerald,
         warning: colors.yellow,
         error: colors.rose,
+        darkbg: "#1e293b",
       },
     },
   },
-  presets: [require('@gits-id/tailwind-config/preset')],
+  presets: [require("@gits-id/tailwind-config/preset")],
 };
