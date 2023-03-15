140a4658699c7868c091326e855759b42ef5849a
Fathan Mubarok <falonez01@gmail.com>
2023-03-15 12:00:28 +0700

    Update Navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -3,10 +3,14 @@
     class="flex justify-between items-center px-[10%] py-8 fixed w-screen top-0 left-0 right-0 text-white bg-slate-600 z-20"
   >
     <nuxt-link to="/" class="text-[1rem] md:text-2xl">Fathan Mubarok</nuxt-link>
-    <div class="text-2xl md:text-4xl text-white flex items-center">
-      <Icon name="mdi:github" />
-      <Icon name="mdi:linkedin" />
-      <Icon name="mdi:instagram" />
+    <div class="text-2xl md:text-4xl text-white flex items-center space-x-2">
+      <a href="https://github.com/falonez"><Icon name="mdi:github" /></a>
+      <a href="https://www.linkedin.com/in/fathan-mubarok-2597171b5/"
+        ><Icon name="mdi:linkedin"
+      /></a>
+      <a href="https://www.instagram.com/falonez15/"
+        ><Icon name="mdi:instagram"
+      /></a>
     </div>
   </div>
 </template>
